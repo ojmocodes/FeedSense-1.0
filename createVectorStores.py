577f933e37062a6954819fb12e75469e31e5f072
OJMO <134063010+ojmocodes@users.noreply.github.com>
2024-01-07 19:36:09 +1300

    succesfully created vector stores for the last 4 weeks of data, streamlit working, can query vector stores, working on routing in handle user input function now

--- createVectorStores.py
+++ createVectorStores.py
@@ -18,29 +18,43 @@ import tempfile
 from datetime import datetime
 
 
+# folder paths for my local setup
+vector_stores_folder_path = "/Users/olivermorris/Documents/GitHub/FeedSense-1.0/vectorStores"
+data_folder_path = "/Users/olivermorris/Documents/GitHub/FeedSense-1.0/csvData"
+
+
+list_of_file_names = [
+    "general_nutrition_info.csv", "Farm_info_from_25-12-2024_to_01-01-2024.csv",
+    "Farm_info_from_18-12-2023_to_25-12-2023.csv",
+    "Farm_info_from_11-12-2023_to_18-12-2023.csv",
+    "Farm_info_from_04-12-2023_to_11-12-2023.csv"
+]
 
 class a_csv_doc():
     def __init__(self, file_name, file_path):
         self.file_name = file_name
         self.file_path = file_path
 
-
-
-nutritional_info = a_csv_doc("Detailed_nutrition_info", "/Users/olivermorris/PycharmProjects/pythonProject2/ask-multiple-pdfs/data/Detailed_nutrition_info.csv")
-
+def csv_to_vectorstore_pipeline(list_of_file_names, data_folder_path):
+    for the_file_name in list_of_file_names:
+        file_name_for_vectorstore = the_file_name.rsplit(".")[0] # don't want file type in name
+        csv_as_doc = a_csv_doc(the_file_name, f"{data_folder_path}/{the_file_name}")
+        csv_text = get_csv_text(csv_as_doc)
+        text_chunks = get_text_chunks(csv_text)
+        vectorstore = create_vectorstore(text_chunks)
+        save_vectorstore_locally(vectorstore, file_name_for_vectorstore, vector_stores_folder_path)
 
 
 # this is only for one csv, but when needed (all owl farm csv's are in data folder) can create a function that loops
 def get_csv_text(the_csv_doc):
-    all_csv_text = ""
-
     loader = CSVLoader(file_path=the_csv_doc.file_path)
     loaded_csvdoc = loader.load()
 
+    all_csv_text = []
     for document in loaded_csvdoc:
         loaded_csvdoc_content = document.page_content
-        all_csv_text += loaded_csvdoc_content
-    return all_csv_text
+        all_csv_text.append(loaded_csvdoc_content)
+    return ''.join(all_csv_text)
 
 
 def get_text_chunks(text):
@@ -67,10 +81,13 @@ def create_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
-def save_vectorstore_locally(vectorstore, desired_name):
-    vectorstore.save_local(f"{desired_name}")
+def save_vectorstore_locally(vectorstore, desired_name, vector_stores_folder_path):
+    vectorstore.save_local(f"{vector_stores_folder_path}/{desired_name}")
 
-def load_vectorstore_locally(vectorstore_name):
+def load_vectorstore_locally(vectorstore_name, vector_stores_folder_path):
     embeddings = OpenAIEmbeddings(openai_api_key = "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")
-    vectorstore = FAISS.load_local(f"{vectorstore_name}", embeddings)
+    vectorstore = FAISS.load_local(f"{vector_stores_folder_path}/{vectorstore_name}", embeddings)
     return vectorstore
+
+# running the final pipeline
+csv_to_vectorstore_pipeline(list_of_file_names, data_folder_path)
---
+++ unit_tests.py
@@ -0,0 +1,133 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.document_loaders import (PyPDFLoader, DataFrameLoader, GitLoader)
+import pandas as pd
+import os
+import tempfile
+from datetime import datetime
+from createVectorStores import load_vectorstore_locally
+
+# I know this is probably the wrong/unorthodox way of doing unit tests
+
+now = datetime.now()
+# print("Current date and time:", now) -> Current date and time: 2024-01-04 15:53:11.348154
+
+# OpenAI info (global), not needed curently because I am explicity giving in params
+OPEN_API_KEY = "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"
+openai_api_key = OPEN_API_KEY
+chat = ChatOpenAI(temperature=0, openai_api_key=OPEN_API_KEY)
+
+vector_stores_folder_path = "/Users/olivermorris/Documents/GitHub/FeedSense-1.0/vectorStores"
+
+def test_querying_vectorstore():
+    vector_stores_folder_path = "/Users/olivermorris/Documents/GitHub/FeedSense-1.0/vectorStores"
+    embeddings = OpenAIEmbeddings(openai_api_key = "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")
+
+    new_db = FAISS.load_local("vectorStores/Farm_info_from_25-12-2024_to_01-01-2024", embeddings)
+
+    docs = new_db.similarity_search("What was the SCC this week?", k=3)
+    print(docs[0].page_content)
+
+
+
+def test_streamlit():
+
+    load_dotenv()
+    st.set_page_config(page_title="Chat with FeedSense - your digital farming brain",
+                       page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with FeedSense!")
+
+    user_question = st.text_input("Ask a question!:")
+
+    if st.button("Process"):
+        # Call the function to process the input
+        processed_input = "hey man just testing here"
+
+        # Display the processed input
+        st.success(f"{processed_input}")
+
+
+# streamlit working
+
+
+def testing_handle_userinput(user_question, now, chat):
+    # route to relevant db
+    # get all results
+    # feed them into completion
+    #return completion.content
+
+    choose_route = chat(
+        messages = [
+        HumanMessage(
+            content=f"""
+            The correct date format for you to always use is day_month_year.
+            Given: {user_question} and {now}, respond, without any other text, with the most accurate of ONE OF THESE TITLES, not including the "" marks:
+            "Farm_info_from_25_12_2024_to_01_01_2024"
+            "Farm_info_from_18_12_2023_to_25_12_2023"
+            "Farm_info_from_11_12_2023_to_18_12_2023"
+            "Farm_info_from_04_12_2023_to_11_12_2023"
+            "general_nutrition_info"
+            """
+        ),
+    ] 
+    )
+    route_choice = choose_route.content
+
+    relevant_vector_store = load_vectorstore_locally(route_choice, vector_stores_folder_path)
+    result = relevant_vector_store.similarity_search(user_question, k=1)[0].page_content
+
+    # here want to get "result" by similarity searching  the route_choice, and then pass that into completion, as part of this the vector store needs to be loaded with the load_vectorstore_locally function in Create_vectorstores
+
+
+    completion = chat(
+        messages = [
+        SystemMessage(
+            content=f"You are a helpful farming assistant. Take a deep breath, work step by step. Don't say 'According to the information provided...' or anything similar. Reply with full context, including assumptions, what figures mean, etc."
+        ),
+        HumanMessage(
+            content=f"Use this information: '{result}' to answer the question: {user_question}. Answer Concisely."
+        ),
+    ]
+    )
+
+    FINAL = completion.content
+    return FINAL
+
+def testing_choose_route(user_question, now, chat):
+    choose_route = chat(
+        messages = [SystemMessage(content = "You are a helpful assistant that chooses an appropriate folder name. The correct date format for you to always use is day_month_year. NEVER respond with anything other than the folder name in your response"),
+        HumanMessage(
+            content=f"""
+            Given: {user_question} and {now}, respond, without any other text, with the most accurate of ONE OF THESE folder names, not including the "" marks:
+            "Farm_info_from_25_12_2024_to_01_01_2024"
+            "Farm_info_from_18_12_2023_to_25_12_2023"
+            "Farm_info_from_11_12_2023_to_18_12_2023"
+            "Farm_info_from_04_12_2023_to_11_12_2023"
+            "general_nutrition_info"
+            """
+        ),
+    ] 
+    )
+    route_choice = choose_route.content
+    return route_choice
+
+print("running")
+print(testing_choose_route("What was the average growth rate 2 weeks ago?", now, chat=chat))
